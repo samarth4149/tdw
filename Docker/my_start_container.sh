21e17ce5b02e1ba0341c3f59e45c804d0f9d1d76
Samarth Mishra <samarth@diva02.pok.ibm.com>
2022-07-13 06:54:28 -0700

    Added my docker stuff

---
+++ Docker/my_start_container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VERSION=$(python tdw_version.py)
+
+# Allow x server to accept local connections
+xhost +local:root
+
+# Run the container
+docker run -it \
+  --gpus '"device=0"' \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /data/samarth:/data/samarth \
+  -e DISPLAY=$DISPLAY \
+  --network host \
+  tdw_latest:latest\
+  ./TDW/TDW.x86_64 $@
+  # /bin/bash -c "Xorg :2 -config /etc/X11/xorg-2.conf & nvidia-smi;TDW/TDW.x86_64 -port=$1;"
