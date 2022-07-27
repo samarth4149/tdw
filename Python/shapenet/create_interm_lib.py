3fd41faedf3aa438955c3c2c520d8f5ab067f714
samarth4149 <samarth4149@gmail.com>
2022-07-26 22:58:40 -0400

    small change

--- Python/shapenet/create_interm_lib.py
+++ Python/shapenet/create_interm_lib.py
@@ -6,9 +6,8 @@ if __name__ == '__main__':
     orig_lib_path = '/data/samarth/tdw_shapenet_core_res100/records.json'
     m = ModelLibrarian(orig_lib_path)
     m.library = '/data/samarth/tdw_shapenet_core_res100/records_interm.json'
-    for r in m.records:
+    for i, r in enumerate(m.records):
         if not os.path.exists(r.get_url()[8:]):
-            print('here')
-            r.do_not_use = True
+            m.records[i].do_not_use = True
             
     m.write(pretty=False)
