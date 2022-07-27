9272f5cdacf50f462ca05de81af5919d5a664c35
samarth4149 <samarth4149@gmail.com>
2022-07-26 23:06:30 -0400

    small change

--- Python/shapenet/create_interm_lib.py
+++ Python/shapenet/create_interm_lib.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 if __name__ == '__main__':
     orig_lib_path = '/data/samarth/tdw_shapenet_core_res100/records.json'
     m = ModelLibrarian(orig_lib_path)
-    for i, r in enumerate(m.records):
+    for r in tqdm(m.records):
         if not os.path.exists(r.get_url()[8:]):
             r.do_not_use = True
             m.add_or_update_record(r, overwrite=True, write=False)
